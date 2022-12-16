cb535f435427f35a5edf19e4104bcc5e6011ffc4
SimonShiki <sinangentoo@gmail.com>
2022-12-16 00:12:26 +0000

    🔧 Add PWA support
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
+import progress from 'vite-plugin-progress'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(), progress()],
   resolve: {
       alias: {
         '@': resolve(__dirname, 'src'),
